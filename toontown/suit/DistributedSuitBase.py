17ba625f73e26f55f81b98ac0c65ae12dc420c30
John Cote <jwcotejr@gmail.com>
2019-11-28 00:08:46 -0500

    suit: cleanup propeller before destroying it

--- toontown/suit/DistributedSuitBase.py
+++ toontown/suit/DistributedSuitBase.py
@@ -170,6 +170,7 @@ class DistributedSuitBase(DistributedAvatar.DistributedAvatar, Suit.Suit, SuitBa
 
     def detachPropeller(self):
         if self.prop:
+            self.prop.cleanup()
             self.prop.removeNode()
             self.prop = None
         if self.propInSound:
