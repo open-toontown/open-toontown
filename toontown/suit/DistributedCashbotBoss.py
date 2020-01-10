7dff988d42c764f3912087d8fcf8fc3339ae4a01
John Cote <jwcotejr@users.noreply.github.com>
2020-01-10 02:16:04 -0500

    suit: fix mata hairy having the wrong dna

--- toontown/suit/DistributedCashbotBoss.py
+++ toontown/suit/DistributedCashbotBoss.py
@@ -101,7 +101,7 @@ class DistributedCashbotBoss(DistributedBossCog.DistributedBossCog, FSM.FSM):
         npc.setPickable(0)
         npc.setPlayerType(NametagGroup.CCNonPlayer)
         dna = ToonDNA.ToonDNA()
-        dna.newToonRandom(11237, 'f', 1)
+        dna.newToonRandom(146392, 'f', 1)
         dna.head = 'pls'
         npc.setDNAString(dna.makeNetString())
         npc.animFSM.request('neutral')
