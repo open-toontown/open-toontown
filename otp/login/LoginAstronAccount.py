d4baf38a5bc3ebd5828c855c3776d4453a64eb80
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2021-11-11 12:40:18 +0000

    login: supportsRelogin should always return 0

--- otp/login/LoginAstronAccount.py
+++ otp/login/LoginAstronAccount.py
@@ -20,8 +20,6 @@ class LoginAstronAccount(LoginBase):
         self.cr.send(datagram)
 
     def supportsRelogin(self):
-        if __debug__:
-            return 1
         return 0
 
     def supportsAuthenticateDelete(self):
