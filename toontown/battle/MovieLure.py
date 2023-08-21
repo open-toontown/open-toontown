e565bad623c0f008d41bc3f3e742ee8eff7d5dee
Chris Darch <comradenapcal@aol.com>
2023-08-21 16:31:19 -0700

    battle: Rename varible as per suggestions.

--- toontown/battle/MovieLure.py
+++ toontown/battle/MovieLure.py
@@ -98,10 +98,10 @@ def __createFishingPoleMultiTrack(lure, dollar, dollarName):
             suitTrack.append(Func(suit.loop, 'neutral'))
             suitTrack.append(Wait(3.5))
             suitName = suit.getStyleName()
-            simulatedPos, simulatedHpr = battle.getActorPosHpr(suit)
-            simulatedPos.setY(simulatedPos.getY() + MovieUtil.SUIT_EXTRA_REACH_DISTANCE)
+            knockBackedPos, knockBackedHpr = battle.getActorPosHpr(suit)
+            knockBackedPos.setY(knockBackedPos.getY() + MovieUtil.SUIT_EXTRA_REACH_DISTANCE)
             if suitName in MovieUtil.largeSuits:
-                moveTrack = lerpSuit(suit, 0.0, reachAnimDuration / 2.5, simulatedPos, battle, trapProp)
+                moveTrack = lerpSuit(suit, 0.0, reachAnimDuration / 2.5, knockBackedPos, battle, trapProp)
                 reachTrack = ActorInterval(suit, 'reach', duration=reachAnimDuration)
                 suitTrack.append(Parallel(moveTrack, reachTrack))
             else:
