4d566e1da9ed595a06045baf0a873daeb3a29d24
John Cote <jwcotejr@gmail.com>
2026-01-30 22:23:46 -0500

    quest|tutorial: Works

---
+++ toontown/tutorial/TutorialBattleManagerAI.py
@@ -0,0 +1,13 @@
+from toontown.battle import BattleManagerAI
+from direct.directnotify import DirectNotifyGlobal
+from toontown.tutorial import DistributedBattleTutorialAI
+
+class TutorialBattleManagerAI(BattleManagerAI.BattleManagerAI):
+
+    notify = DirectNotifyGlobal.directNotify.newCategory('TutorialBattleManagerAI')
+
+    def __init__(self, air):
+        BattleManagerAI.BattleManagerAI.__init__(self, air)
+        self.battleConstructor = DistributedBattleTutorialAI.DistributedBattleTutorialAI
+        
+        
